36bbe3615523339cef6d674a7b3e8b216929921c
Jhonata Vinicius Da Silva Araujo <52162636+jhony2488@users.noreply.github.com>
2020-05-05 13:31:30 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const vm = new Vue({
+  el: '#app',
+  data: {
+    binary: '00001',
+    decimal: '10',
+  },
+  methods: {
+    calculoBinaryDecimal() {
+      setInterval(() => {
+        var jhony = this.binary
+        this.decimal = parseInt(jhony, 2)
+      }, 1000)
+    },
+  },
+  computed: {
+    calculo1() {
+      this.calculoBinaryDecimal()
+    },
+  },
+  created() {
+    this.calculo1
+  },
+})
